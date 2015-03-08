410296f1f34e63b22195dbf1925cf8654090e683
sshell0611 <scott.shellenhamer@gmail.com>
2015-03-08 15:54:30 -0400

    pentagon

--- graphics/assignment5/drawlib1.js
+++ graphics/assignment5/drawlib1.js
@@ -16,20 +16,20 @@ Vector3.prototype = {
 		       },
 }
 
-function Vector4(x, y, z, p) {
+function Vector4(x, y, z, w) {
  this.x = 0;
  this.y = 0;
  this.z = 0;
- this.p = 1;
- this.set(x, y, z, p);
+ this.w = 1;
+ this.set(x, y, z, w);
 }
 
 Vector4.prototype = {
-         set : function(x, y, z, p) {
+         set : function(x, y, z, w) {
 		          if (x !== undefined) this.x = x;
 		          if (y !== undefined) this.y = y;
 		          if (z !== undefined) this.z = z;
-		          if (p !== undefined) this.p = p;
+		          if (w !== undefined) this.w = w;
 
 		       },
 }
@@ -173,6 +173,8 @@ function Square(x,y,sz) {
  this.nEdges   = 0;
  this.nVertex  = 0;
  this.size	   = 0;
+ this.x		   = 0;
+ this.y		   = 0;
  this.border   = 'black';
  this.fill	   = 'white';
  this.init(x,y,sz);
@@ -183,14 +185,17 @@ Square.prototype = {
 		       	if (x  == undefined) throw "Undefined start x coordinate";
 		       	if (y  == undefined) throw "Undefined start y coordinate";
 		       	if (sz == undefined) throw "Undefined size";
+				this.x = x;
+				this.y = y;
 				this.nEdges  = 4;
 				this.nVertex = 4;
 				this.size	 = sz;
+				var hsz = sz/2;
 				//ignore the z coordinate and use z to represent 1 for a homogeneous point
-				var vtx0 = new Vector3(x,   y   , 1);
-				var vtx1 = new Vector3(x+sz,y   , 1);
-				var vtx2 = new Vector3(x   ,y+sz, 1);
-				var vtx3 = new Vector3(x+sz,y+sz, 1);
+				var vtx0 = new Vector3(x-hsz, y-hsz, 1);
+				var vtx1 = new Vector3(x+hsz, y-hsz, 1);
+				var vtx2 = new Vector3(x-hsz, y+hsz, 1);
+				var vtx3 = new Vector3(x+hsz, y+hsz, 1);
 				this.vertices = [vtx0, vtx1, vtx2, vtx3];
 				this.edges    = [[0,1], [1,3], [3,2], [2,0]];
 		    },
@@ -231,9 +236,7 @@ Square.prototype = {
 			},
 
 		clone: function() {
-				var x  = this.vertices[0].x;
-				var y  = this.vertices[1].y;
-				var sq = new Square(x, y, this.size);
+				var sq = new Square(this.x, this.y, this.size);
 				sq.setBorder(this.getBorder());
 				sq.setFill(this.getFill());
 				return sq;
@@ -262,9 +265,116 @@ Square.prototype = {
 				return sq;
 			},
 
-		getX: function() {
-				return this.vertices[0].x;	
+}
+
+
+//here x/y is the center point, r is the "radius" from the center
+function Pentagon(x,y,r) {
+ this.vertices = [];
+ this.edges    = []; 
+ this.nEdges   = 0;
+ this.nVertex  = 0;
+ this.size	   = 0;
+ this.x		   = 0;
+ this.y 	   = 0;
+ this.r		   = 0;
+ this.border   = 'black';
+ this.fill	   = 'white';
+ this.init(x,y,r);
+}
+
+Pentagon.prototype = {
+		init: function(x, y, r) {
+		       	if (x == undefined) throw "Undefined start x coordinate";
+		       	if (y == undefined) throw "Undefined start y coordinate";
+		       	if (r == undefined) throw "Undefined radius";
+				this.x = x;
+				this.y = y;
+				this.r = r;
+				this.nEdges  = 5;
+				this.nVertex = 5;
+				this.size	 = r;
+
+				var c1 = r * Math.cos((2 * Math.PI) / 5) ;
+				var c2 = r * Math.cos(Math.PI / 5);
+				var s1 = r * Math.sin((2 * Math.PI) / 5);
+				var s2 = r * Math.sin((4 * Math.PI) / 5);
+
+				//ignore the z coordinate and use z to represent 1 for a homogeneous point
+				var vtx0 = new Vector3(-s2+x, -c2+y, 1);
+				var vtx1 = new Vector3( s2+x, -c2+y, 1);
+				var vtx2 = new Vector3(-s1+x,  c1+y, 1);
+				var vtx3 = new Vector3( s1+x,  c1+y, 1);
+				var vtx4 = new Vector3(  x, y+r, 1);
+				this.vertices = [vtx0, vtx1, vtx2, vtx3, vtx4];
+				this.edges    = [[0,1], [1,3], [3,4], [4,2], [2,0]];
+		    },
+
+		numEdges: function() {
+				return this.nEdges;	
 			},
+
+		getVertex: function (v) {
+				if (v > 4)	throw "Invalid vertex, must be from 0 to 4";
+				return this.vertices[v];
+			},
+
+		setVertex: function(v, vertex) {
+				if (v > 4)	throw "Invalid vertex, must be from 0 to 4";
+				this.vertices[v] = vertex;
+			},
+
+		getEdge: function (e) {
+				if (e > 4)	throw "Invalid vertex, must be from 0 to 4";
+				return this.edges[e];
+			},
+
+		setBorder: function(color) {
+				this.border = color;	
+			},
+
+		getBorder: function() {
+				return this.border;	
+			},
+
+		setFill: function(color) {
+				this.fill = color;	
+			},
+
+		getFill: function() {
+				return this.fill;	
+			},
+
+		clone: function() {
+				var sq = new Square(this.x, this.y, this.size);
+				sq.setBorder(this.getBorder());
+				sq.setFill(this.getFill());
+				return sq;
+		    },
+
+		//transform using a matrix 
+		//true/false flag to change the matrix
+		transform: function(M, inplace) {
+				var obj;
+				if (inplace == true)	
+					obj = this;
+				else
+					obj = this.clone();
+
+				//console.log(this.getX());
+
+				for (v = 0; v < this.nVertex; v++)
+				{
+					var newVtx = new Vector3(0, 0, 0);
+					M.transform(obj.getVertex(v), newVtx);
+					obj.setVertex(v, newVtx);
+				}
+
+				//console.log(this.getX());
+
+				return obj;
+			},
+
 }
 
 /////////////////////////////////////////////////////////////
@@ -313,6 +423,33 @@ function drawSquare(square, canvas, w, h) {
 	canvas.stroke();
 }
 
+function drawShape(shape, canvas, w, h) {
+
+	canvas.strokeStyle = shape.getBorder();
+	canvas.fillStyle = shape.getFill();
+
+	//move to the first vertex
+	canvas.beginPath();
+
+	var vtx1 = shape.getVertex(0);
+	var x = toViewportX(vtx1.x, w);
+	var y = toViewportY(vtx1.y, w, h);
+
+	canvas.moveTo(x, y);
+
+	//loop through all of the edges
+	for (e=0; e < shape.numEdges(); e++)
+	{
+		var edge = shape.getEdge(e);
+		var vtx  = shape.getVertex(edge[1]);
+		var x    = toViewportX(vtx.x, w);
+		var y    = toViewportY(vtx.y, w, h);
+		canvas.lineTo(x, y);
+	}
+
+	canvas.fill();
+	canvas.stroke();
+}
 
 
 /////////////////////////////////////////////////////////////

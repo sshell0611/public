bf93a88d3848489130de3e87733f75e74371a625
sshell0611 <scott.shellenhamer@gmail.com>
2015-05-05 19:55:10 -0400

    updates

--- graphics/final/HammerVis_.js
+++ graphics/final/HammerVis_.js
@@ -2,6 +2,9 @@
 //
 var HAMMER = HAMMER || {};
 
+
+
+
 /***********************************************************
  * Chart 
  ***********************************************************/
@@ -9,40 +12,31 @@ var HAMMER = HAMMER || {};
 var Chart = function(type, loc, sz) {
 	this.chartType = type;
 	this.loc   = loc; 	//vector2 of upper left corner
-	this.size  = sz;		//vector2 of size
+	this.size  = sz;	//vector2 of size
 	this.xAxis = null;
 	this.yAxis = null;
 	this.border = null;
 	this.series = [];
 	this.factory = null;
 	this.borderEps = 0.15;
-	this.screenRangeX = null;
-	this.screenRangeY = null;
-	this.margin = 0.20;
+	this.screenRngX = null;
+	this.screenRngY = null;
 
 	console.log('chart created');
 
 	//border
 	//backcolor
-	this.init(type, loc, sz);
-}
+	this.init();
+};
 
 Chart.prototype = {
 		
-		init : function(type, loc, sz) {
-			this.chartType = type;
-			this.loc = loc;
-			this.size = sz;
+		init : function() {
 			this.createBorder();
-			this.screenRangeX = new AxisRange(-sz.x/2+this.margin, sz.x/2-this.margin);
-			this.screenRangeY = new AxisRange(-sz.y/2+this.margin, sz.y/2-this.margin);
+			this.screenRngX = new AxisRange(this.loc.x-this.sz.x, this.loc.x);
+			this.screenRngY = new AxisRange(this.loc.y, this.loc.y+this.sz.y);
 			this.yAxis = new Axis('vertical', this);
 			this.xAxis = new Axis('horizontal', this);
-			//hack for now
-			if (this.chartType == 'line') {
-				this.yAxis.onlypositive = true;
-				this.xAxis.onlypositive = true;
-			}
 			this.factory = new ChartRendererFactory();
 		},
 
@@ -54,26 +48,22 @@ Chart.prototype = {
 			this.border = new THREE.EdgesHelper(cube, 0x7e7e7e);
 		},
 
-		addSeries: function(s) {
-			var len = this.series.length;
-			this.series[len] = s;
-		},
-
 		getScreenRange : function(side) {
-			if (side == 'horizontal') {
+			if (side == 'x' || side == 'horizontal') {
 				return this.screenRangeX;	
 			}
-			else if (side == 'vertical') {
+			else if (side == 'y' || side == 'vertical') {
 				return this.screenRangeY;
 			}
 			return null;
 		},
 
-		getMargin: function() {
-			return this.margin;	
+		addSeries: function(s) {
+			var len = this.series.length;
+			this.series[len] = s;
 		},
 
-		_globalDataRangeX: function() {
+		_globalRangeX: function() {
 			var min = 10000;
 			var max = -10000;
 			for (s = 0; s < this.series.length; s++)
@@ -83,11 +73,10 @@ Chart.prototype = {
 				min = Math.min(xMin, min);
 				max = Math.max(xMax, max);
 			}
-			//return new THREE.Vector2(min, max);
-			return new AxisRange(min, max);
+			return new THREE.Vector2(min, max);
 		},
 
-		_globalDataRangeY: function() {
+		_globalRangeY: function() {
 			var min = 10000;
 			var max = -10000;
 			for (s = 0; s < this.series.length; s++)
@@ -97,8 +86,7 @@ Chart.prototype = {
 				min = Math.min(yMin, min);
 				max = Math.max(yMax, max);
 			}
-			//return new THREE.Vector2(min, max);
-			return new AxisRange(min, max);
+			return new THREE.Vector2(min, max);
 		},
 
 
@@ -108,8 +96,8 @@ Chart.prototype = {
 			var renderer = this.factory.createRenderer(this.chartType);
 
 			//we will have multiple series so have to find overall max/min
-			var xRng = this._globalDataRangeX(); 
-			var yRng = this._globalDataRangeY();
+			xRng = this._globalRangeX(); 
+			yRng = this._globalRangeY();
 
 			this.xAxis.setDataRange(xRng);
 			this.yAxis.setDataRange(yRng);
@@ -157,41 +145,23 @@ LineChartRenderer.prototype = {
 		chart.yAxis.render(scene);
 		//render series
 
-		var xScreenRng = chart.getScreenRange('horizontal');
-		var yScreenRng = chart.getScreenRange('vertical');
+		xFactor = chart.xAxis.getScaleFactor();
+		yFactor = chart.yAxis.getScaleFactor();
 
 		for (s = 0; s < chart.series.length; s++) {
 
 			//color needs to be different for each
-			var material = new THREE.LineBasicMaterial( { color: 0x0000ff, linewidth:1.0, linecap:'bevel', linejoin:'bevel' });
+			var material = new THREE.LineBasicMaterial( { color: 0x0000ff });
 			var geometry = new THREE.Geometry();
 
 			for (p = 0; p < chart.series[s].numPoints(); p++) {
 				var pt = chart.series[s].getPoint(p);
-				
-				//console.log(pt);
-				//console.log('before: ' + pt.arg + ', ' + pt.val);
+				pt.scale(xFactor, yFactor, 1);
 
-				var screenPt = pt.clone();
-				//var x = chart.xAxis.axisRange.convertTo(xScreenRng, pt.arg);
-				//var y = chart.yAxis.axisRange.convertTo(yScreenRng, pt.val);
-				var x = chart.xAxis.convertPointToScreen(xScreenRng, pt.arg);
-				var y = chart.yAxis.convertPointToScreen(yScreenRng, pt.val);
+				geometry.vertices.push(pt.getDrawVector());
 
-				screenPt.set(x, y);
-				//console.log(screenPt);
-
-				//console.log('after: ' + screenPt.arg + ', ' + screenPt.val);
-				//first scale to the axis
-				//pt.scale(chart.xAxis.)
-				//then scale to the screen
-				//pt.scale(xFactor, yFactor, 1);
-
-				geometry.vertices.push(screenPt.getVector());
-
-				var circle = screenPt.getDrawable();
-				//console.log(circle);
-				scene.add(circle);
+				//var mesh = pt.getDrawable();
+				//scene.add(mesh);
 			}
 
 			var line = new THREE.Line(geometry, material);
@@ -216,8 +186,8 @@ BubbleChartRenderer.prototype = {
 		var max = -10000;
 		for (s = 0; s < chart.series.length; s++)
 		{
-			wMax = Math.abs(chart.series[s].maximumW());
-			wMin = Math.abs(chart.series[s].minimumW());
+			wMax = chart.series[s].maximumW();
+			wMin = chart.series[s].minimumW();
 			min = Math.min(wMin, min);
 			max = Math.max(wMax, max);
 		}
@@ -235,30 +205,19 @@ BubbleChartRenderer.prototype = {
 		chart.yAxis.render(scene);
 		//render series
 
-		var xScreenRng = chart.getScreenRange('horizontal');
-		var yScreenRng = chart.getScreenRange('vertical');
-
 		xFactor = chart.xAxis.getScaleFactor();
 		yFactor = chart.yAxis.getScaleFactor();
 
 		wFactor = this._globalWeightFactor(chart);
 
-		//console.log('x scale factor: ' + xFactor);
-		//console.log('y scale factor: ' + yFactor);
+		console.log('x scale factor: ' + xFactor);
+		console.log('y scale factor: ' + yFactor);
 
 		for (s = 0; s < chart.series.length; s++) {
 			for (p = 0; p < chart.series[s].numPoints(); p++) {
 				var pt = chart.series[s].getPoint(p);
-				
-				var screenPt = pt.clone();
-				screenPt.scale(wFactor);
-
-				var x = chart.xAxis.convertPointToScreen(xScreenRng, pt.arg);
-				var y = chart.yAxis.convertPointToScreen(yScreenRng, pt.val);
-
-				screenPt.set(x, y);
-
-				var mesh = screenPt.getDrawable();
+				pt.scale(xFactor, yFactor, wFactor);
+				var mesh = pt.getDrawable();
 				scene.add(mesh);
 			}
 		}
@@ -273,9 +232,7 @@ BubbleChartRenderer.prototype = {
 var AxisRange = function(lower, upper) {
 	this.lower = lower;
 	this.upper = upper;	
-	this.buffer = 0; //buffer is a percentage
-	this.padding = 0;
-}
+};
 
 AxisRange.prototype = {
 
@@ -288,34 +245,16 @@ AxisRange.prototype = {
 
 	//normalize a value on this axis to the 0 -> 1 axis
 	normalize: function(value) {
-		//console.log('old axis lower: ' + this.lower);
-		//console.log('old axis range: ' + this.getRange());
 		return (value - this.lower) / this.getRange();
 	},
 
 	//convert point on this axis to the axis given
-	convertTo : function(axisRng, value) {
-		var normpt = this.normalize(value);
-		//console.log('norm pt: ' + normpt);
-		//console.log('new axis range: ' + axisRng.getRange());
-		//console.log('new axis lower: ' + axisRng.lower);
-		var newpt = normpt * axisRng.getRange()	+ axisRng.lower;
+	convertTo: function(axisRng, value) {
+		val normpt = this.normalize(value);
+		val newpt = normpt * axisRng.getRange()	+ axisRng.lower;
 		return newpt;	
 	},
-
-	addBuffer : function(buffer) {
-		this.buffer = buffer;
-		this.upper = this.upper + (this.upper * buffer);
-		this.lower = this.lower - (this.lower * buffer);
-	},
-
-	addPadding : function(padding) {
-		this.padding = padding;
-		this.upper = this.upper + padding;
-		this.lower = this.lower - padding;
-	}
 }
-
 
 /***********************************************************
  * Axis 
@@ -323,19 +262,18 @@ AxisRange.prototype = {
 // pass position and length
 var Axis = function(type, chart) {
 	this.chartRef  = null;
-	this.axisRange = null;
-	this.dataRange = null;
+	this.dataRng   = null;
+	this.axisRng   = null;
+	this.length = 0;;
 	this.thickness = 2.0;
 	this.unit = "";
+	this.init();
 	this.line = null;
 	this.geometry = null;
 	this.material = null;
 	this.type = type;
-	this.rangeMin = 0;
-	this.rangeMax = 0;
-	this.buffer = .10;
-	this.padding= 1.0;
-	this.onlypositive = false;
+	this.buffer = 1.5;
+	this.autoscale = true;
 	this.init(type, chart);
 }
 
@@ -344,78 +282,81 @@ Axis.prototype = {
 		init : function (type, chart) {
 			this.type = type;
 			if (chart !== 'undefined') {
-				this.chartRef  = chart;
-				this.axisRange = chart.getScreenRange(type);
+				this.chartRef = chart;
+				this.axisRng = this.chartRef.getScreenRange(type);
+				//initialize to the screen range if available
+				this._create();
 			}
 		},
 
-		_create : function() {
-		
-			//var margin = this.chartRef.getMargin();
-			var margin = 0;
-			var start, end;
-			if (this.type == 'vertical') {
-				if (this.onlypositive == true) {
-					
-					var screenRng = this.chartRef.getScreenRange('horizontal');
-					start = new THREE.Vector3(screenRng.lower+margin, this.axisRange.lower+margin, 0);
-					end   = new THREE.Vector3(screenRng.lower+margin, this.axisRange.upper-margin, 0);
+		_create: function () {
+			if (this.axisRng !== null) { 
+				this.material = new THREE.LineBasicMaterial( { color: 0x7e7e7e, linewidth:this.thickness});
+				this.geometry = new THREE.Geometry();
+
+				var start, end;
+				var tmpRange;
+
+				if (this.autoscale == true && this.dataRng != null) {
+					tmpRange.upper = this.dataRng.upper;
+					tmpRange.lower = this.dataRng.lower;
 				}
 				else {
-					start = new THREE.Vector3(0, this.axisRange.lower, 0);
-					end   = new THREE.Vector3(0, this.axisRange.upper, 0);
-				}
-			}
-			else {
-				if (this.onlypositive == true) {
-
-					var screenRng = this.chartRef.getScreenRange('vertical');
-					start = new THREE.Vector3(this.axisRange.lower+margin, screenRng.lower+margin, 0);
-					end   = new THREE.Vector3(this.axisRange.upper-margin, screenRng.lower+margin, 0);
+					tmpRange.upper = this.axisRng.upper;
+					tmpRange.lower = this.axisRng.lower;	
 				}
 
+				var screenRng = this.chartRef.getScreenRange(this.type);
+				start = tmpRange.convertTo(tmpRange.
+
+				if (type == 'vertical') {
+					start = new THREE.Vector3(0, -this.axisRng.lower, 0);
+					end   = new THREE.Vector3(0, this.axisRng.upper, 0);
+				}
 				else {
-					start = new THREE.Vector3(this.axisRange.lower, 0, 0);
-					end   = new THREE.Vector3(this.axisRange.upper, 0, 0);
+					start = new THREE.Vector3(-this.axisRng.lower, 0, 0);
+					end   = new THREE.Vector3(this.axisRng.upper, 0, 0);
 				}
+
+				this.geometry.vertices.push( start, end );
+				this.line = new THREE.Line( this.geometry, this.material );
 			}
-			this.material = new THREE.LineBasicMaterial( { color: 0x7e7e7e, linewidth:this.thickness});
-			this.geometry = new THREE.Geometry();
-			this.geometry.vertices.push( start, end );
-			this.line = new THREE.Line( this.geometry, this.material );
+		},
+
+		_getFitAxis : function() {
+			var fitgeometry = new THREE.Geometry();
+			fitgeometry.vertices.push( start, end );
+			var fitline = new THREE.Line( fitgeometry, this.material );
+			return fitline;	
+		},
+
+		setAxisRange : function(rng) {
+			this.axisRng = rng;	
 		},
 
 		setDataRange : function(rng) {
-			this.dataRange = rng;
-			this.dataRange.addPadding(this.padding);
-			//this.axisRange = rng;
-			var v1 = Math.abs(rng.lower);
-			var v2 = Math.abs(rng.upper);
-			var val = Math.max(v1, v2);
-			this.rangeMin = -val * this.buffer*100.0;
-			this.rangeMax = val * this.buffer*100.0;	
-		},
-
-		scalePoint: function(pt) {
-			var newpt = pt.clone();
-			//var x = this.
-		},
-
-		convertPointToScreen: function(screenRng, ptval) {
-			var sp = this.dataRange.convertTo(screenRng, ptval);
-			return sp;
+			this.dataRng = new AxisRange(rng.x, rng.y);
+			//var min = Math.abs(rng.x);
+			//var max = Math.abs(rng.y);
+			//var val = Math.max(v1, v2);
+			//this.lowerBound = -val * this.buffer;
+			//this.upperBound = val * this.buffer;	
 		},
 
 		getScaleFactor : function() {
 			var range = this.rangeMax - this.rangeMin;
-			return (this.axisRange.getRange() / range);
+			return (this.length / range);
 		},
 
 		//render type, leave blank for whole axis
 		//render fit will adjust the display just for the visible range 
-		render : function (scene) {
-			this._create();
-			scene.add(this.line);
+		render : function (scene, type) {
+			if (type == 'fit') {
+				scene.add(this._getFitAxis());
+			}
+			else {
+				scene.add(this.line);
+			}
 		},
 
 		update : function () {
@@ -460,8 +401,8 @@ Series.prototype = {
 		},
 
 		add: function(pt) {
-			//if (pt.type !== this.type && pt.type !== 'basic')
-			//	throw 'Exception: Point type does not match series type';
+			if (pt.type !== this.type && pt.type !== 'basic')
+				throw 'Exception: Point type does not match series type';
 			var len = this.points.length;	
 			this.points[len] = pt;
 			this._evaluateMaxMin(pt);
@@ -509,29 +450,19 @@ var SeriesPoint = function(x, y, type) {
 	this.type = 'basic';
 	this.arg = x;	//these are the actual values
 	this.val = y;	//these are the actual values
+	this.x   = x;	//these are the scaled values for drawing
+	this.y	 = y;	//these are the scaled valeus for drawing
 	this.tag = "";
 	this.color = null;
 	this.mesh = null;
-	this.init(x, y, type);
+	this.init(type);
 }
 
 SeriesPoint.prototype = {
 
-		init : function(x, y, type) {
-			this.arg = x;
-			this.val = y;
-			this.color = 0x0000ff;
-		    	if (type !== undefined) this.type = type;
-			var radius = 0.025;
-			var geom = new THREE.CircleGeometry(radius, 32);
-			var mat  = new THREE.MeshBasicMaterial({ color : this.color } );
-			//ambient  : 0,
-			//emissive : 0x0000ff,
-			//color    : 0x0000ff,
-			//specular : 0x101010,
-			//shininess: 50 });
-			this.mesh = new THREE.Mesh(geom, mat);
-			//this.mesh.material.shading = THREE.SmoothShading;
+		init : function(type) {
+		    if (type !== undefined) this.type = type;
+			color = 0x0000ff;
 		},
 
 		set : function(arg, val) {
@@ -539,26 +470,17 @@ SeriesPoint.prototype = {
 			this.val = val;		
 		},
 
-		clone : function() {
-			var cln = new SeriesPoint(this.arg, this.val, this.type);
-			cln.color = this.color;
-			cln.tag = this.tag;	
-			cln.mesh = this.mesh;
-			return cln;
-		},
-
 		getDrawable: function() {
-			this.mesh.position.set(this.arg, this.val, 0);
 			return this.mesh;	
 		},
 
-		/*scale : function(xScale, yScale, wScale) {
+		scale : function(xScale, yScale, wScale) {
 			this.x = this.arg * xScale;
 			this.y = this.val * yScale;
 			if (this.mesh !== null) {
 				this.mesh.position.set(this.x, this.y, 0);
 			}
-		},*/
+		},
 
 		getVector: function() {
 			return new THREE.Vector3(this.arg, this.val, 0);
@@ -576,7 +498,6 @@ SeriesPoint.prototype = {
  ***********************************************************/
 var BubblePoint = function(x, y, w) {
 	SeriesPoint.call(this, x, y);
-	this.baserad = 0.10;
 	this.type   = "bubble";
 	this.weight = w;
 	this.z = w;		//this is the scaled value
@@ -587,7 +508,8 @@ BubblePoint.prototype = Object.create(SeriesPoint.prototype);
 BubblePoint.prototype.constructor = BubblePoint;
 
 BubblePoint.prototype.init = function() {
-	var geom = new THREE.SphereGeometry(this.baserad, 32, 32);
+	var radius = 0.25;
+	var geom = new THREE.SphereGeometry(radius, 32, 32);
 	var mat  = new THREE.MeshPhongMaterial({
 		ambient  : 0,
 		emissive : 0x0000ff,
@@ -601,30 +523,17 @@ BubblePoint.prototype.init = function() {
 BubblePoint.prototype.set = function(arg, val, wt) {
 	this.arg = arg;
 	this.val = val;
-	if (wt !== 'undefined') {
-		this.weight = wt;
-	}
+	this.weight = wt;
 };
 
-BubblePoint.prototype.scale = function(/*xScale, yScale,*/wScale) {
-	//this.x = this.arg * xScale;
-	//this.y = this.val * yScale;
-	//this.mesh.position.set(this.x, this.y, 0);
-	var middle = wScale / 2.0;		//the middle is our base radius
-	//var normW = Math.sqrt(this.weight / wScale);
-	var normW = Math.sqrt(this.weight / middle);
+BubblePoint.prototype.scale = function(xScale, yScale, wScale) {
+	this.x = this.arg * xScale;
+	this.y = this.val * yScale;
+	this.mesh.position.set(this.x, this.y, 0);
+	var normW = Math.sqrt(this.weight / wScale);
 	console.log(normW);
 	this.z = normW;
 	this.mesh.scale.set(normW, normW, normW);
 };
 
-BubblePoint.prototype.clone = function() {
-	var cln = new BubblePoint(this.arg, this.val, this.weight);
-	cln.type = this.type;
-	cln.color = this.color;
-	cln.tag = this.tag;	
-	cln.mesh = this.mesh;
-	cln.z = this.z;
-	return cln;
-};
 
